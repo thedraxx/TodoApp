bde8298f08aef5198d060a14b2c651bbf2a9014d
thedraxx <55867157+thedraxx@users.noreply.github.com>
2021-08-20 14:15:38 -0300

    Guardar-eliminar-Citas-Del-Storage
    AsyncStorage agregado para guardar y eliminar datos

--- App.js
+++ App.js
@@ -1,115 +1,152 @@
-import React,{useState} from 'react';
-import {Text,StyleSheet,View,FlatList, TouchableHighlight,ScrollView,Platform,TouchableWithoutFeedback,Keyboard} from 'react-native';
+import React, {useState, useEffect} from 'react';
+import {
+  Text,
+  StyleSheet,
+  View,
+  FlatList,
+  TouchableHighlight,
+  Platform,
+  TouchableWithoutFeedback,
+  Keyboard,
+} from 'react-native';
 import Cita from './componentes/Cita.js';
 import Formulario from './componentes/Formulario.js';
+import AsyncStorage from '@react-native-community/async-storage';
 
 const App = () => {
-
+  //definir el state
+  const [citas, setCitas] = useState([]);
   const [mostrarform, guardarMostrarForm] = useState(false);
 
-  //definir el state 
-  
-  const [citas, setCitas] = useState([])
+  useEffect(() => {
+    const obtenerCitasStorage = async () => {
+      try {
+        const citasStorage = await AsyncStorage.getItem('citas');
+        if (citasStorage) {
+          setCitas(JSON.parse(citasStorage));
+        }
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    obtenerCitasStorage();
+  }, []);
 
   //Elimianar Pacientes del state
   const eliminarPaciente = id => {
-    setCitas((citasActuales) => {
-      return citasActuales.filter(cita => cita.id !== id);
-    })
-  }
+    const citasFiltradas = citas.filter(cita => cita.id !== id);
+    setCitas(citasFiltradas);
+    guardarCitasStorage(JSON.stringify(citasFiltradas));
+  };
 
-  //mustra u oculta el formulario 
+  //mustra u oculta el formulario
 
   const mostrarFormulario = () => {
     guardarMostrarForm(!mostrarform);
-  }
-
+  };
 
   //Ocultar el teclado
 
   const cerrarTeclado = () => {
     Keyboard.dismiss();
-  }
+  };
 
+  // Almacenar las citas en el storage
+
+  const guardarCitasStorage = async citasJSON => {
+    try {
+      await AsyncStorage.setItem('citas', citasJSON);
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
   return (
-    
-      <TouchableWithoutFeedback onPress={()=>cerrarTeclado()}>
-      <View style={styles.contenedor}> 
+    <TouchableWithoutFeedback onPress={() => cerrarTeclado()}>
+      <View style={styles.contenedor}>
         <Text style={styles.titulo}>ADMINISTRADOR DE CITAS</Text>
 
         <View>
-                <TouchableHighlight onPress={()=> mostrarFormulario() } style={styles.btnmostrarForm}>
-                <Text style={styles.TextoMostrarForm}>{mostrarform ? 'cancelar crear cita' : 'crear nueca cita'}</Text>
-                </TouchableHighlight>
+          <TouchableHighlight
+            onPress={() => mostrarFormulario()}
+            style={styles.btnmostrarForm}>
+            <Text style={styles.TextoMostrarForm}>
+              {mostrarform ? 'cancelar crear cita' : 'crear nueca cita'}
+            </Text>
+          </TouchableHighlight>
         </View>
 
-          <View style={ styles.contenido}> 
-       
-            {mostrarform ? (
+        <View style={styles.contenido}>
+          {mostrarform ? (
             <React.Fragment>
               <Text style={styles.titulo}> Crear una nueva cita</Text>
-                <Formulario 
-                  citas={citas}
-                  setCitas={setCitas}
-                  guardarMostrarForm={guardarMostrarForm}
-                />
-            </React.Fragment> 
-            ) : (
+              <Formulario
+                citas={citas}
+                setCitas={setCitas}
+                guardarMostrarForm={guardarMostrarForm}
+                guardarCitasStorage={guardarCitasStorage}
+              />
+            </React.Fragment>
+          ) : (
             <React.Fragment>
-              <Text style={styles.titulo}> {citas.length > 0 ? 'Administra tus citas' : 'No hay citas agregar una' }</Text>
+              <Text style={styles.titulo}>
+                {' '}
+                {citas.length > 0
+                  ? 'Administra tus citas'
+                  : 'No hay citas agregar una'}
+              </Text>
               <FlatList
-                style={ styles.listado}
+                style={styles.listado}
                 data={citas}
-                renderItem={ ({item})=> <Cita item={item} eliminarPaciente={eliminarPaciente}/>}
+                renderItem={({item}) => (
+                  <Cita item={item} eliminarPaciente={eliminarPaciente} />
+                )}
                 keyExtractor={cita => cita.id}
               />
             </React.Fragment>
-            )}
-          </View>
+          )}
+        </View>
       </View>
-      </TouchableWithoutFeedback>
-    
+    </TouchableWithoutFeedback>
   );
 };
 
 const styles = StyleSheet.create({
-  contenedor:{
-    backgroundColor:'#FFFF',
-    flex:1
+  contenedor: {
+    backgroundColor: '#FFFF',
+    flex: 1,
   },
 
- titulo:{
-  textAlign:'left',
-  marginTop: Platform.OS === 'ios' ? 40 : 20 ,
-  marginLeft: 50,
-  fontSize:15,
-  fontWeight: 'bold',
-  marginBottom:20,
- },
+  titulo: {
+    textAlign: 'left',
+    marginTop: Platform.OS === 'ios' ? 40 : 20,
+    marginLeft: 50,
+    fontSize: 15,
+    fontWeight: 'bold',
+    marginBottom: 20,
+  },
 
- contenido: {
-   flex: 1,
-   marginHorizontal:'2.5%'
- },
+  contenido: {
+    flex: 1,
+    marginHorizontal: '2.5%',
+  },
 
- listado:
- {
-   flex:1,
- },
+  listado: {
+    flex: 1,
+  },
 
- btnmostrarForm:{
-  padding: 10,
-  backgroundColor:'red',
-  marginVertical:5,
-  marginHorizontal:30
-},
+  btnmostrarForm: {
+    padding: 10,
+    backgroundColor: 'red',
+    marginVertical: 5,
+    marginHorizontal: 30,
+  },
 
-TextoMostrarForm:{
-  color:'#FFF',
-  fontWeight:'bold',
-  textAlign:'center'
-},
+  TextoMostrarForm: {
+    color: '#FFF',
+    fontWeight: 'bold',
+    textAlign: 'center',
+  },
 });
 
 export default App;
